935adbbf136ccf84878a34927de5741d176b5747
Juan Salmerón Moya <jnssterrass@gmail.com>
2016-10-08 21:30:48 +0200

    Host by url.

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ function initMediaDevices(){
 }
 
 function initSource() {
-    sourcePeer = new Peer({host: "http://juansalmeron.com",port: 9000});
+    sourcePeer = new Peer({host: "juansalmeron.com",port: 9000});
     sourcePeer.on('open', function(id) {
         sourcePeerId = id;
         console.log('Source peer ID is: ' + sourcePeerId);
